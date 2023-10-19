f7b7eb6ab308d1c4d3f6a54daa7952eadbbfe224
TJ-Griffin <tjgriffin77@gmail.com>
2023-10-19 10:16:52 -0400

    Created base of cookie scraper

--- util/scrape_wiki.py
+++ util/scrape_wiki.py
@@ -1,3 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 
+base_url = "https://cookierunkingdom.fandom.com/wiki/"
+
+class Cookie:
+	'''
+	Cookie object to store all information of each of cookie
+	'''
+
+	def __init__(self, name, link):
+		'''
+		Assign the known names
+
+		params:
+			name (str) : name of cookie
+			link (str) : url destination to cookie
+		'''
+		self.name = name
+		self.link = link
+
+	def find_info(self):
+		'''
+		Search through cookie's link to scrape the information.
+		Sets: Gender, 
+		'''
+		pass
+
+
+def scrape_cookies():
+	'''
+	Function to scrape the cookies information.
+
+	returns:
+		cookies (List[Cookie]) : a list of Cookie objects
+	'''
+	r = requests.get(base_url + "List_of_Cookies")
+
+	soup = BeautifulSoup(r.content, 'html5lib')
+	cookies = []
+
+	for table in soup.findAll("table", attrs = {'class' : 'fandom-table'}):
+		for tr in table.findAll("tr"):
+			name = tr.th.b.a['title']
+			link = tr.th.b.a['href']
+			c = Cookie(name, link)
+			cookies.append(c)
+	
+	return cookies
+
+
+cookies = scrape_cookies()
