a7d6c96e1c403230c2d9ff13e8dd93a55c06f3f0
Thomas <tjgriffin77@gmail.com>
2024-01-02 17:00:59 -0500

    Gacha Templating init

--- gacha.py
+++ gacha.py
@@ -5,3 +5,35 @@ import aiomysql
 import datetime
 import random
 
+
+class Interaction(commands.Cog):
+    '''
+    Interaction with the users (slash commands, other than )
+
+    functions -->
+        - a single pull (interacts with the gacha pull using slash)
+        - a multi pull (interacts with the gacha pull using slash)
+        - check balance
+        - daily login bonus
+    '''
+    pass
+
+
+class Gacha:
+    '''
+    gacha logic and computations
+
+    functions -->
+        - a pull function for gacha (cost, check if can afford, result)
+
+    '''
+    pass
+
+
+class Cookie:
+    '''
+    An individual item (cookie) stats of cookie
+
+
+    '''
+    pass
