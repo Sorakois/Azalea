4c6b6fc7e1bbdc4272790dbeb34ebb732c5dbdeb
Skylar <bleau.skylar@gmail.com>
2024-12-27 19:49:17 -0500

    added proper solo guessing game compatibility, starting Azalea personality development

---
+++ psyche.py
@@ -0,0 +1,11 @@
+# Use ollama:
+# general ver -> https://github.com/ollama/ollama
+# python ver -> https://github.com/ollama/ollama-python
+
+# "Train" with something like this:
+'''
+code
+code
+code
+prompt(f"Write a response to the following in this mindset: {prompt}")
+'''
