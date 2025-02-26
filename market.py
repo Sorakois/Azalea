c8b632b74c03baaed23d35a7ecbc1e3bfa90ab96
skylarbleau <bleau.skylar@gmail.com>
2025-02-26 00:42:33 -0500

    setup essence/gem check for trading command

--- market.py
+++ market.py
@@ -15,7 +15,10 @@ class Business(commands.Cog):
     Input another user, what items to trade
     Other user will input trade items
     Both will confirm, exchange done!
+    
+    NOTE: Store locally user1 VS user2 requests!
     '''
+    
     @discord.app_commands.checks.cooldown(1, 30)
     @app_commands.command(name="trade", description="Trade your items with others!")
     async def number_guess(self, interaction : discord.Interaction, other_user : discord.User):
@@ -26,30 +29,96 @@ class Business(commands.Cog):
         if other_user.id == 1160998311264796714 or other_user.id == 1082486461103878245:
                 await interaction.response.send_message(f"Azalea is busy :(", ephemeral=True)
         else:
-            # User 1 will send a reques to User 2 on what they would like to trade!
-            user1_request = await interaction.channel.send("Please type what you would like to trade!\n\nType either the name of a character or amount of essence/gems to trade.\nTo trade multiple items, seperate with commas (,)\nAdd 'G' or 'E' after a number for gem/essence")
-            def check(message: discord.Message):
-                return message.author.id == member.id and message.channel.id == interaction.channel.id
-
-            # Grab user's raw input
-            msg = await interaction.client.wait_for('message', check=check, timeout=30.0)
-            item_to_trade = msg.content
+            # go twice, one time for each user
+            for index_user in range(2):
             
-            # The input might contain multiple items... break them appart.
-            item_to_trade = item_to_trade.strip()
-            list_to_trade = item_to_trade.split(",")
-            # Remove user inputted ","
-            for request in list_to_trade:
-                request.replace(",","")
+                # Ask what to trade! User 1 makes the request first.
                 
-            try:
-                async with self.bot.db.acquire() as conn:
-                    async with conn.cursor() as cursor:
-                        # Check if the user actually owns what they want to trade!
-                        if any(isinstance(item, (int)) for item in list_to_trade):
-                            # Essence is being traded!
-                            await cursor.execute(f"SELECT USER_ESSENCE FROM USER WHERE USER_ID = %s", member.id)
-                            essence_check = await cursor.fetchall()
+                # Keep looping until valid input
+                while(True):
+                    
+                    await interaction.channel.send("Please type what you would like to trade!\n\nType either the name of a character or amount of essence/gems to trade.\nTo trade multiple items, seperate with commas (,)\nAdd 'G' or 'E' after a number for gem/essence")
+                    def check(message: discord.Message):
+                        if index_user == 0:
+                            return message.author.id == member.id and message.channel.id == interaction.channel.id # Fisrt User
                         else:
-                            # An item/character is being traded!
-                            a=1
+                            return message.author.id == other_user.id and message.channel.id == interaction.channel.id # Second User
+
+                    # Grab user's raw input
+                    msg = await interaction.client.wait_for('message', check=check, timeout=30.0)
+                    item_to_trade = msg.content
+                    
+                    # The input might contain multiple items... break them appart.
+                    item_to_trade = item_to_trade.strip()
+                    list_to_trade = item_to_trade.split(",")
+                    # Remove user inputted ","
+                    for request in list_to_trade:
+                        request.replace(",","")
+                    
+                    try:
+                        async with self.bot.db.acquire() as conn:
+                            async with conn.cursor() as cursor:
+                                
+                                # Check if the user actually owns what they want to trade!
+                                
+                                # Execute if a number is followed by a letter
+                                if any(isinstance(list_to_trade[i], int) and isinstance(list_to_trade[i + 1], str) and list_to_trade[i + 1].isalpha() 
+                                for i in range(len(list_to_trade) - 1)):
+                                    
+                                    # Figure out if gem or essence is being traded.
+                                    trading_essence = [item for item in list_to_trade if item.endswith('E')]
+                                    trading_gems = [item for item in list_to_trade if item.endswith('G')]
+                                    
+                                    if trading_essence is not None:
+                                        await cursor.execute(f"SELECT USER_ESSENCE, USER_GEMS FROM USER WHERE USER_ID = %s", (member.id if index_user == 0 else other_user.id))
+                                        essence_gems = await cursor.fetchone()
+                                        
+                                        # Seperate gems and essence
+                                        essence_check = essence_gems[0]
+                                        gems_check = essence_gems[1]
+                                        
+                                        if trading_essence > essence_check:
+                                            # STOP! User input error.
+                                            await interaction.channel.send(f"You are requesting to trade more essence than you own!\nYou own {essence_check} essence, yet requested to trade {trading_essence} essence.\nPlease try again.")
+                                            continue # Go to the top and try again.
+                                        if trading_gems > gems_check:
+                                            # STOP! User input error.
+                                            await interaction.channel.send(f"You are requesting to trade more gems than you own!\nYou own {gems_check} gems, yet requested to trade {trading_gems} gems.\nPlease try again.")
+                                            continue # Go to the top and try again.
+                                        
+                                        #store essence/gems via what user
+                                        if index_user == 0:
+                                            # First user
+                                            user1_gems = trading_gems
+                                            user1_ess = trading_essence
+                                        else:
+                                            # Second user
+                                            user2_gems = trading_gems
+                                            user2_ess = trading_essence
+                                            
+                                    else:
+                                        await interaction.channel.send(f"Invalid request: Ensure only G or E is added to the end of numbers.\nPlease try again.")
+                                        continue # Go to the top and try again.
+                                else:
+                                    # An item/character is being traded!
+                                    
+                                    '''
+                                    PROCESS:
+                                        Check if the item exists
+                                        Check if the user owns the item
+                                        Assign items as a tuple(), no need to mutate
+                                    '''
+                                    
+                                    
+                    except ValueError as valE:
+                        # An error is thrown, likely because the user did not enter a valid name
+                        await interaction.channel.send(f"ERROR: One of the characters you entered do not exist.\nPlease try again.")
+                        continue # Go to the top and try again.
+                    except:
+                    # General error catch
+                        await interaction.channel.send(f"ERROR: Invalid input.\nPlease try again.")
+                        continue # Go to the top and try again.
+                    
+                    # User 1 has successfully requested! Enter loop again for User 2. Break after both succeed.
+                    # This will only be executed if the continues do not.
+                    break
