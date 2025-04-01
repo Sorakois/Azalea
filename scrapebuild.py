0635e5f4a9b6c48f894555d1a9b21fbf7357ac9a
skylarbleau <bleau.skylar@gmail.com>
2025-04-01 15:46:14 -0400

    more progress on scrape. about to implement comparing build table elements vs wiki valid chars

--- scrapebuild.py
+++ scrapebuild.py
@@ -27,3 +27,30 @@ build_soup = BeautifulSoup(open(sheet_url))
 # We can convert with list() if needed, though!
 # Output should look like [<tr></tr>, <tr></tr>, ...]
 buildBowl = build_soup.findAll('tr')
+
+# Now, lets parse through all the <tr> elements properly
+# Let's use another site to compare valid characters
+valid_char_check_url = "https://honkai-star-rail.fandom.com/wiki/Character/List"
+checking_soup = BeautifulSoup(open(valid_char_check_url))
+# Every table and data we need to check CURRENTLY is under the class: article-table sortable jquery-tablesorter
+check_tables = checking_soup.find_all("table", class_="article-table sortable jquery-tablesorter")
+# Now we have two tables
+# checking_soup = [Released Chars], [Upcoming Chars]
+
+# Each character name currently looks like this:
+# <a href="/wiki-link" class="charname"> CHAR NAME HERE </a>
+# Let's get all <a> inside the table to parse
+valid_char_names = []
+
+for table in check_tables:
+    links = table.find_all('a', href=True)
+    
+    # Now we have an <a> element. Let's get the value inside.
+    for link in links:
+        link_text = link.get_text()
+        valid_char_names.extend(link_text)
+        
+# Now we have a valid set of characters to compare with for our Build Table!
+# Let's now use it to parse through with index manipulation
+        
+        
