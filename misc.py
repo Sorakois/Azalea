9a93152fa167351d68133ef5a969e6007a348bee
skylarbleau <bleau.skylar@gmail.com>
2025-04-08 13:08:08 -0400

    fixesss

--- misc.py
+++ misc.py
@@ -208,10 +208,10 @@ class MiscCMD(commands.Cog):
                 em.add_field(name="Trace Priority: ", value=f"{HSRBuildInfo[1]}", inline=False)
                 em.add_field(name="Best LCs: ", value=f"{HSRBuildInfo[2]}", inline=True)
                 em.set_thumbnail(url=interaction.user.guild.icon.url)
-                em.add_field(name="Recommended Stats: ", value=f"{HSRBuildInfo[0]}", inline=True)
                 em.add_field(name="Best Relics: ", value=f"{HSRBuildInfo[3]}", inline=False)
                 em.add_field(name="Best Planar Relics: ", value=f"{HSRBuildInfo[4]}", inline=True)
                 em.add_field(name="Best Gear Mainstats: ", value=f"{HSRBuildInfo[5]}", inline=False)
+                em.add_field(name="Recommended Stats: ", value=f"{HSRBuildInfo[0]}", inline=True)
                 '''TODO: Make this into page 2 later'''
                 #em.add_field(name="Notes: ", value=f"{HSRBuildInfo[7]}", inline=True)
                 
